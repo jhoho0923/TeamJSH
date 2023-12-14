f5311866b9e3109963f9a659ee6a77281e6184ad
jhoho82 <jhoho82@gmail.com>
2023-12-14 17:19:03 +0900

    RRRRRR

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-	    // add 함수구현  
+	    // add 함수구현add(인자값1, 인자값2) 
  	   int my_add(int number1, int number2){
 		if (number1 < 0 or number2 < 0)
 			return;
