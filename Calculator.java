17e8358c49729ec691519ae84040ef075744333f
jhoho82 <jhoho82@gmail.com>
2023-12-14 17:31:38 +0900

    &*((

--- Calculator.java
+++ Calculator.java
@@ -7,7 +7,8 @@ public class Calculator {
  	   int my_add(int number1, int number2){
 		if (number1 < 0 or number2 < 0)
 			return;
-		return number1+number2;
+		return number1+number2; 
+		// 정수 값을 반환한다.
 	}
     }
 }
