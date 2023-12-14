4979fe3922d3bb7b8eefa79e05ca47761bc5eb2d
jhoho82 <jhoho82@gmail.com>
2023-12-14 17:06:30 +0900

    .

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+	    // add 함수구현  
  	   int my_add(int number1, int number2){
 		if (number1 < 0 or number2 < 0)
 			return;
