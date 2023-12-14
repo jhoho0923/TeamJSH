171adb1d72f24049bf8bfb33a3ab80ce6b46550f
hisame103018 <hisame103018@gmail.com>
2023-12-14 16:29:39 +0900

    팀원1 작업완료

--- Calculator.java
+++ Calculator.java
@@ -3,5 +3,10 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+ 	   int my_add(int number1, int number2){
+		if (number1 < 0 or number2 < 0)
+			return;
+		return number1+number2;
+	}
     }
 }
