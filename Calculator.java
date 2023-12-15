b55f03ba102d5172134ffa6c0824f22713e0196b
jhoho82 <jhoho82@gmail.com>
2023-12-15 10:51:49 +0900

    9888

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+//2
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+	    // add 함수구현add(인자값1, 인자값2) 
+ 	   int my_add(int number1, int number2){
+		if (number1 < 0 or number2 < 0) // 들어오는 값이 크면(조건절구문)
+			return;
+		return number1+number2; 
+		// 정수 값을 반환한다.
+
+	}
+    }
+}
